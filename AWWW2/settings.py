cc3602a298a1726fb58dcb25d0d21f13d3e2eac0
pawelgadzinski <pawelgadzinski@gmail.com>
2023-06-02 13:13:38 +0200

    a

--- AWWW2/settings.py
+++ AWWW2/settings.py
@@ -131,3 +131,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://54.37.139.148:1235']
